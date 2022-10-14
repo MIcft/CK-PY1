c7a2852891eb3697c69636204c30050acc5cb5ae
MIcft <62951378+MIcft@users.noreply.github.com>
2022-10-14 23:26:42 +0300

    Add files via upload

---
+++ Лабораторная работа 3/Task1.py
@@ -0,0 +1,11 @@
+src = True and True or False and False
+# result = True and True or False
+# Избавляемся от логического оператора И
+# result = True and True
+# Избавляемся от логического оператора ИЛИ
+# result = True
+# Избавляемся от логического оператора И
+
+result = True
+
+print(src == result)
