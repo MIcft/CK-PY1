0693495ee84fb2dd2eb86555cf4f807155f617f7
Ivanov Petr <62951378+MIcft@users.noreply.github.com>
2022-10-20 15:36:28 +0300

    Revert "Add files via upload"

--- Лабораторная работа 3/Task1.py
+++
@@ -1,9 +0,0 @@
-src = not False and True or False and not True
-# Избавляемся от логического оператора NOT
-result = True and True or False and False
-# Избавляемся от логического оператора И
-result = True or False
-# Избавляемся от логического оператора ИЛИ
-result = True
-
-print(src == result)
