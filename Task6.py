92d54f8fbc635daf36f714e400af0a4bed926856
Ivanov Petr <62951378+MIcft@users.noreply.github.com>
2022-10-20 15:43:58 +0300

    Delete Task6.py

--- Task6.py
+++
@@ -1,7 +0,0 @@
-list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-first_max = max(list_numbers)
-index_first_max = list_numbers.index(first_max)
-last_number = list_numbers[-1]
-list_numbers[index_first_max] = last_number
-list_numbers[-1] = first_max
-print(list_numbers)
