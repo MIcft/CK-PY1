ba1fa45c320a3052bad3c5a7215119c81ee48791
Ivanov Petr <62951378+MIcft@users.noreply.github.com>
2022-10-28 13:10:45 +0300

    Лабораторная работа 5

---
+++ Лаборатораня работа 5/Task_5.py
@@ -0,0 +1,15 @@
+# Написать функцию для генерации случайных паролей заданной длины n (по умолчанию 8 символов). В качестве алфавита следует использовать:
+#
+# Буквы верхнего регистра: A - Z
+# Буквы нижнего регистра: a - z
+# Цифры: 0 - 9
+# Для того чтобы сгенерировать случайную выборку, следует использовать функцию sample из модуля random.
+# TODO написать функцию генерации случайных паролей
+from random import sample
+def get_random_password() -> list:
+    list_symbol = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    list_ = sample(list_symbol, 8)
+
+    return list_
+
+print(get_random_password())
