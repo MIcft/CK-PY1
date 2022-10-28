f62d6bef94686ab52dcb84b152fca0e9cb3b4dc8
Ivanov Petr <62951378+MIcft@users.noreply.github.com>
2022-10-28 15:45:48 +0300

    Update Task_1.py

--- Лабораторная работа 4/Task_1.py
+++ Лабораторная работа 4/Task_1.py
@@ -1,40 +1,28 @@
-#
-# Напишите функцию, которая будет принимать строку, а возвращать словарь с частотой каждого символа.
-#
-# Чтобы не считать символы разного регистра два раза, с помощью метода строки lower перевести строку в нижний регистр
-#
-# С помощью метода строки isalpha проверяем, что символ является буквой.
-# Напишите вторую функцию, которая принимает словарь символов.
-# Функция должна вернуть новый словарь, где количество каждого элемента заменено на процентное отношение ко всем символам
-
-
-
-
 def get_count_char(str_):
-    separate_list = str_.lower().split()
-    separate_list.sort()
-    list_ = ' '.join(separate_list)
+    str_ = str_.lower()
     alphabet_dict = {}
-    for letter in list_:
-        if letter in alphabet_dict :
+
+    for letter in str_:
+        if letter in alphabet_dict:
             alphabet_dict[letter] += 1
         elif letter.isalpha():
             alphabet_dict[letter] = 1
     return alphabet_dict
 
+
 def percent_char(dict_):
-    # Расчет сколько всего символов
     count = sum(dict_.values())
     for letter, value in dict_.items():
-        dict_[letter] = round(value/count, 3)
-    return dict_
+        dict_[letter] = round(value / count, 3)
+    print(dict_)
+
 
 main_str = """
     Данное предложение будет разбиваться на отдельные слова. 
     В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов. 
     Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
 """
-dict_ =  get_count_char(main_str)
+dict_ = get_count_char(main_str)
 
-print(dict_)
+print(get_count_char(main_str))
 print(percent_char(dict_))
