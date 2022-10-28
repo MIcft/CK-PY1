c4501c3a24507c218983b19ec67f89e22ca8d936
Ivanov Petr <62951378+MIcft@users.noreply.github.com>
2022-10-28 14:39:38 +0300

    Update Task_2.py

--- Лабораторная работа 4/Task_2.py
+++ Лабораторная работа 4/Task_2.py
@@ -6,8 +6,7 @@
 def delete(list_, index=None):
     if index is None:
         index = len(list_)-1  # Автор кода не знает что такое list_[-1]
-    list_del = list_[:index]+list_[index+1:]
-    return list_del
+    return list_[:index]+list_[index+1:]
 
 print(delete([0, 0, 1, 2], index=0))  # [0, 1, 2]
 print(delete([0, 1, 1, 2, 3], index=1))  # [0, 1, 2]
